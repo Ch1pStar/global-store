1852bec32a2d5506eb05a8209524d311169aa874
Nedelin Yordanov <n.yordanov@dopamine.bg>
2018-02-08 10:39:06 +0200

    feat: Move initDebug function to utils

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
-import {get, goHot, clearLocalState} from './util'
+import {get, initDebug} from './util'
 import middleware from './middleware/index'
 import reducer from './reducers/index'
 
-import {createUpdateFromStringAction} from './actions/update'
 import createView from './view';
 
 function loaded (data) {
@@ -12,19 +11,6 @@ function loaded (data) {
   createView(data, store);
 
   store.dispatch({type: 'init'});
-}
-
-function initDebug(data, store) {
-  const debug = document.querySelector('.debug-text')
-  const btn = document.querySelector('.debug-update')
-  const updateFromString = createUpdateFromStringAction(store)
-  const clearCacheBtn = document.querySelector('.debug-clear-cache')
-
-  debug.value = JSON.stringify({success: true, result: data})
-  debug.addEventListener('paste', (e) => updateFromString(e.clipboardData.getData('Text')))
-
-  btn.addEventListener('click', () => updateFromString(debug.value))
-  clearCacheBtn.addEventListener('click', clearLocalState)
 }
 
 document.addEventListener('DOMContentLoaded', () => get(loaded))
--- src/js/util.js
+++ src/js/util.js
@@ -1,3 +1,5 @@
+import {createUpdateFromStringAction} from './actions/update'
+
 export function get (callback, url = 'src/mock/settings.json') { window.fetch(url).then((res) => res.json()).then((data) => callback(data.result)) }
 
 export function goHot () {
@@ -12,4 +14,18 @@ export function clearLocalState () {
 
 export function currentTime() {
 	return window.performance.now() << 0
+}
+
+
+export function initDebug(data, store) {
+  const debug = document.querySelector('.debug-text')
+  const btn = document.querySelector('.debug-update')
+  const updateFromString = createUpdateFromStringAction(store)
+  const clearCacheBtn = document.querySelector('.debug-clear-cache')
+
+  debug.value = JSON.stringify({success: true, result: data})
+  debug.addEventListener('paste', (e) => updateFromString(e.clipboardData.getData('Text')))
+
+  btn.addEventListener('click', () => updateFromString(debug.value))
+  clearCacheBtn.addEventListener('click', clearLocalState)
 }
