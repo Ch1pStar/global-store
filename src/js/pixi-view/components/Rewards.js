ebef3c28e58f59cca127e639f514a87c1b8d8d9c
Nedelin Yordanov <n.yordanov@dopamine.bg>
2018-02-13 19:36:11 +0200

    tmp: Use tmp asset for rewards title

--- src/js/pixi-view/components/Rewards.js
+++ src/js/pixi-view/components/Rewards.js
@@ -6,15 +6,15 @@ export default class Rewards extends PIXI.Container {
     super();
     this._state = state
 
-    const text = new PIXI.Text('Rewards', {fill: 0xffffff, fontSize: 18});
+    // const text = new PIXI.Text('Rewards', {fill: 0xffffff, fontSize: 18});
 
-    this.addChild(text);
-    this._title = text;
+    this._title = PIXI.Sprite.fromFrame('d');
+    this.addChild(this._title);
 
     const cnt = this.cnt = new PIXI.Container();
 
     const rewardsText = new PIXI.Text(this._rewardsString, {fill: 0xffffff, fontSize: 16});
-    cnt.y = text.height + 10;
+    cnt.y = this._title.height + 10;
 
     cnt.addChild(rewardsText);
     this.addChild(cnt);
@@ -28,7 +28,7 @@ export default class Rewards extends PIXI.Container {
     this._state = val
     this.dirty = true
 
-    this._title.style.fill = val.time.isHot ? 0xff77aa : 0xffffff;
+    // this._title.style.fill = val.time.isHot ? 0xff77aa : 0xffffff;
     const rewardsText = this.cnt.children[0];
 
     rewardsText.text = this._rewardsString;
